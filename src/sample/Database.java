3b622cdac7d7ac3b98b3b8870c1153e0487bae62
shahbajsingh <shahbajs987@gmail.com>
2021-12-18 23:36:54 -0500

    Initial commit

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -1,4 +1,30 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Database {
+
+    public static void createNewDatabase() {
+
+        String url = "jdbc:sqlite:C:/sqlite/db/database.db";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        createNewDatabase();
+    }
 }
+
--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -6,42 +6,51 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.TouchEvent;
 
+import javax.swing.*;
+
 public class LoginController {
 
     @FXML
-    private JFXTextField login;
+    private JFXTextField btnLogin;
 
     @FXML
-    private JFXPasswordField password;
+    private JFXPasswordField btnPassword;
 
     @FXML
-    private JFXHamburger touch;
+    private JFXHamburger hmbMenu;
 
     @FXML
-    private JFXButton submit;
+    private JFXButton btnSubmit;
 
     @FXML
-    void handleEnter(KeyEvent event) {
+    private JFXButton btnForgotPass;
+
+    @FXML
+    private Label labelUserWarning, labelPasswordWarning;
+
+
+    @FXML
+    void handlePasswordEnter(KeyEvent event) {
 
         if(event.getCode().equals(KeyCode.ENTER)){
-            password.setText("");
-            login.setText("");
+            btnPassword.setText("");
+            btnLogin.setText("");
 
 
         }
     }
 
     @FXML
-    void handleEnterL(KeyEvent event) {
-        
-        if(event.getCode().equals(KeyCode.ENTER)){
-            password.setText("");
-            login.setText("");
+    void handleLoginEnter(KeyEvent event) {
 
+        if(event.getCode().equals(KeyCode.ENTER)){
+            btnPassword.setText("");
+            btnLogin.setText("");
 
         }
     }
@@ -50,11 +59,48 @@ public class LoginController {
     @FXML
     void handleHover(TouchEvent event) {
 
+
+
+    }
+
+    @FXML
+    void handleForgotPass(ActionEvent event)
+    {
+        if(event.getSource().equals(btnForgotPass))
+        {
+
+
+
+        }
     }
 
     @FXML
     void handleSubmit(ActionEvent event) {
+        if(event.getSource().equals(btnSubmit))
+        {
 
+            if(btnLogin.getText().equals("") || btnPassword.getText().equals("")) {
+                if (btnLogin.getText().equals("")) {
+                    labelUserWarning.setText("Can't be empty");
+
+                }
+                if (btnPassword.getText().equals("")) {
+                    labelPasswordWarning.setText("Can't be empty");
+
+                }
+                btnLogin.setText("");
+                btnPassword.setText("");
+
+            }
+            else{
+                labelUserWarning.setText("");
+                labelPasswordWarning.setText("");
+
+
+
+            }
+
+        }
     }
 
 }
